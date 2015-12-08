8791ae0ad6ecb3deaa5aad0c34938363cd6995b0
migcar429 <migcar429@gmail.com>
2015-12-08 15:06:54 -0800

    played around with sqlite, fuzzywuzzy

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-'''import sqlite3
+import sqlite3
 
 conn = sqlite3.connect('example.db')
 
-c = conn.cursor()'''
+c = conn.cursor()
+
+'''c.execute('CREATE TABLE keywords (keyword text PRIMARY KEY, search_vol int)')
+c.execute('CREATE TABLE related_keywords (related_keyword text PRIMARY KEY, search_vol int, keyword text, FOREIGN KEY(keyword) REFERENCES keywords(keyword))')'''
+
+#c.execute("DELETE FROM keywords WHERE keyword='toxicologist'")
+#c.execute("INSERT INTO keywords VALUES ('toxicologist', 500)")
+c.execute('SELECT * FROM keywords')
+c.execute('SELECT * FROM related_keywords')
+print c.fetchall()
+
 '''
 import requests
 import os.path
@@ -28,11 +38,11 @@ data = r.json()
 pprint.pprint(data)
 '''
 
-from fuzzywuzzy import fuzz
+'''from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-foo = 'toxicologist'
-bar = 'copperheaist'
+foo = 'hematologist'
+bar = 'hematologist/oncologist'
 
 
 print fuzz.ratio(foo, bar)
@@ -41,5 +51,5 @@ print fuzz.partial_ratio(foo, bar)
 
 print fuzz.token_sort_ratio(foo, bar)
 
-print fuzz.token_set_ratio(foo, bar)
+print fuzz.token_set_ratio(foo, bar)'''
 
