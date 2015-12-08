c23f049a17aacd7f3e50596483d33813cbe5295f
migcar429 <migcar429@gmail.com>
2015-12-08 13:16:42 -0800

    Finished lookuprelated().

--- zocdocgrep.py
+++ zocdocgrep.py
@@ -3,14 +3,17 @@ import json
 import requests
 import os.path
 from xlwings import Workbook, Range
-from fuzzwuzzy import fuzz, process
+from fuzzywuzzy import fuzz, process
 
 # Read API key from .txt file. 
 f = open('/Users/Mig/Desktop/grepkey.txt', 'r') 
 grepkey = f.read()
 f.close()
 
-search_vol_threshold = 2000
+# Set this to the minimum search volume that the client cares about.
+search_vol_threshold = 1500
+
+taboo_words = ['salary', 'what is', 'what does']
 
 
 
@@ -52,6 +55,13 @@ def lookuprelated(specialties):
 
 	#related_words_suff = ['s near me', 'near me', 'clinic', 'clinic near me', 'locations']
 	
+	# For testing: check all returned from 'related' query.
+	'''for specialty in specialties:
+		r = requests.get('http://api.grepwords.com/related?apikey={0}&q={1}&results={2}'.format(grepkey, specialty, 10))
+		r = r.json()
+		for kw in r:
+			print kw['keyword'], '   ', kw['gms']'''
+
 	for specialty in specialties:
 		r = requests.get('http://api.grepwords.com/related?apikey={0}&q={1}&results={2}'.format(grepkey, specialty, 10))
 		r = r.json()
@@ -59,19 +69,16 @@ def lookuprelated(specialties):
 			if kw['keyword'].lower() == specialty.lower():
 				pass
 			# Filter search volume + makes sure that the keyword (all words in search term) is found within the related term.
-			elif fuzzy.token_set_ratio(kw['keyword'].lower(), specialty.lower()) == 100 and kw['gms'] > search_vol_threshold:
-				related_vol_data.update({kw['keyword']:kw['gms']})
+			elif fuzz.token_set_ratio(kw['keyword'].lower(), specialty.lower()) == 100 and kw['gms'] >= search_vol_threshold:
+				for taboo_word in taboo_words:
+					if taboo_word in kw['keyword']:
+						break
+				else:
+					related_vol_data.update({kw['keyword']:kw['gms']})
 
 
 	pprint.pprint(related_vol_data)
 	return related_vol_data
-
-'''
-r = requests.get('http://api.grepwords.com/related?apikey={0}&q={1}'.format(grepkey, 'urgent care'))
-data = r.json()
-
-pprint.pprint(data)
-'''
 
 
 def main():
